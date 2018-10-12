2aecd9df5f5bb215be833b70b3e0f0f89fceb1b3
Piotr <piotrgoralewicz25@gmail.com>
2018-10-12 15:25:36 +0200

    pierwsze polaczenie

--- WARSZTAT_3/src/main/java/servlets/exercise.java
+++ WARSZTAT_3/src/main/java/servlets/exercise.java
@@ -1,11 +1,16 @@
 package servlets;
 
+import classes.Exercise;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @WebServlet(name = "exercise", urlPatterns = "/exercise")
 public class exercise extends HttpServlet {
@@ -14,6 +19,45 @@ public class exercise extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try(
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/warsztat2?useTimezone=true&serverTimezone=GMT&useSSL=false&characterEncoding=utf8",
+                        "root", "coderslab");
+        ){
+            Exercise[] exercises = Exercise.loadAllExercises(conn);
+            response.getWriter().append(
+                    "<a href='/main'>Strona główna</a><br/>" +
+                    "<a href='/addExercise'>dodanie zadania</a><br/>" +
+                    "<a href='/editExercise'>edycja zadania</a><br/>" +
+                    "<a href='/deleteExercise'>usuniecie zadania</a><br/>"
+                    );
+            response.getWriter().append("" +
+                    "<table>" +
+                    "   <tr>" +
+                    "       <td>id</td>" +
+                    "       <td>title</td>" +
+                    "       <td>description</td>" +
+                    "   </tr>"
+            );
+            for(Exercise e : exercises){
+                response.getWriter().append("" +
+                    "   <tr>" +
+                    "       <td>" + e.getId() + "</td>" +
+                    "       <td>" + e.getTitle() + "</td>" +
+                    "       <td>" + e.getDescription() + "</td>" +
+                    "   </tr>"
+                );
+            }
+            response.getWriter().append(
+                    "</table>"
+            );
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
--- WARSZTAT_3/target/WARSZTAT_3-1.0-SNAPSHOT/exercise.jsp
+++ WARSZTAT_3/target/WARSZTAT_3-1.0-SNAPSHOT/exercise.jsp
@@ -1,10 +1,33 @@
-<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>exercise</title>
+    <style>
+        table td{
+            border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
+    <a href='/main'>Strona główna</a><br/>
+    <a href='/addExercise'>dodanie zadania</a><br/>
+    <a href='/editExercise'>edycja zadania</a><br/>
+    <a href='/deleteExercise'>usuniecie zadania</a><br/>
 
+    <table>
+        <tr>
+            <td>id</td>
+            <td>title</td>
+            <td>description</td>
+        </tr>
+        <c:forEach items="${ex}" var="e">
+            <tr>
+                <td>${e.id}</td>
+                <td>${e.title}</td>
+                <td>${e.description}</td>
+            </tr>
+        </c:forEach>
+    </table>
 </body>
 </html>
