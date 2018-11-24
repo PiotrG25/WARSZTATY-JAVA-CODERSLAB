c33a9ca053c50b033f34b53841a3ebdf2e53964d
Piotr <piotrgoralewicz25@gmail.com>
2018-11-24 17:20:43 +0100

    Article

--- WARSZTAT_6/src/main/java/pl/coderslab/entity/Article.java
+++ WARSZTAT_6/src/main/java/pl/coderslab/entity/Article.java
@@ -1,5 +1,24 @@
 package pl.coderslab.entity;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "articles")
 public class Article {
     /*Wpisy: Każdy użytkownik może stworzyć nieograniczoną liczbę wpisów. Maksymalna długość wpisu to 140 znaków.*/
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull @NotBlank
+    private String description;
+
+    private Long user_id;
+
+    private LocalDateTime date;
 }
